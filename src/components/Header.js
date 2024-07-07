b3de50419cf07c90326fdb9f3c2c155ec565d0d6
Arthur Hsu <efyah7@exmail.nottingham.ac.uk>
2024-07-07 20:12:10 +0100

    Add coffee logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,18 @@ function Header() {
     <header className="bg-primary text-primary-content shadow-lg">
       <div className="container mx-auto px-4 py-4">
         <nav className="flex justify-between items-center">
-          <Link to="/" className="text-2xl font-bold">Coffee Finder</Link>
+          <Link to="/" className="flex items-center">
+            <img 
+              src={process.env.PUBLIC_URL + '/coffee.ico'} 
+              alt="Coffee Finder Logo" 
+              className="h-8 w-auto mr-2"
+            />
+            <div className="flex flex-col">
+              <span className="text-2xl font-bold">CoffeeWithMe</span>
+              <span className="text-sm -mt-1">by Arthur Hsu</span>
+            </div>
+
+          </Link>
           <ul className="flex space-x-4">
             <li><Link to="/" className="hover:text-secondary">Home</Link></li>
             <li><Link to="/about" className="hover:text-secondary">About</Link></li>
